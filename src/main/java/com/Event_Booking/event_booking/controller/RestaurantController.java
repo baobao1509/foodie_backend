9a036a4b56898fa8988bb0526ae2221386772dfa
Bao <giabaodongthanh@gmail.com>
2026-06-12 18:24:06 +0700

    asdas

--- src/main/java/com/Event_Booking/event_booking/controller/RestaurantController.java
+++ src/main/java/com/Event_Booking/event_booking/controller/RestaurantController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ve/restaurants")
+@RequestMapping("/api/v1/restaurants")
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantService restaurantService;
